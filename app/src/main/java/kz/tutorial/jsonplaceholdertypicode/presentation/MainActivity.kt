0a7ff2ad61333e2566fba936c59cc15879e67cd8
Yerbol Kabzitov <kabzitov.erbol@mail.ru>
2024-02-14 22:09:38 +0600

    Add bottom navigation

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/MainActivity.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/MainActivity.kt
@@ -2,6 +2,9 @@ package kz.tutorial.jsonplaceholdertypicode.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kz.tutorial.jsonplaceholdertypicode.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
